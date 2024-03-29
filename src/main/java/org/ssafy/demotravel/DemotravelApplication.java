dedbf01b8bd587d786fed638fe340c9eed0bcb80
kwondh5217 <trewq231@naver.com>
2024-03-29 17:13:17 +0900

    Initial commit

---
+++ src/main/java/org/ssafy/demotravel/DemotravelApplication.java
@@ -0,0 +1,13 @@
+package org.ssafy.demotravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotravelApplication.class, args);
+	}
+
+}
